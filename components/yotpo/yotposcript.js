05d8bf5db3a9dda66935127236b57c42ab33f83b
root <fullstackdeveloper52@outlook.com>
2015-05-05 16:18:34 -0700

    yotposcript.js updated

--- components/yotpo/yotposcript.js
+++
@@ -1,1 +0,0 @@
-(function e(){var e= document.createElement("script");e.type="text/javascript",e.async=true,e.src="//staticw2.yotpo.com/gzLifCZ7CnkFZTx4G1LaYq2lKf3l5GCCSQy6FivI/widget.js";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)})();
