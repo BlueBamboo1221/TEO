04395049d4cedb58f134ad7812337fd2b8fb3054
root <fullstackdeveloper52@outlook.com>
2015-07-10 16:18:00 -0700

    index.js updated

--- styles/index.js
+++
@@ -1,5 +0,0 @@
-export { default as applySettings } from './settings';
-export { globalStyleObject, globalStyleString } from './global-styles';
-export { themeColors, socialColors } from './colors';
-export { default as shadows } from './shadows';
-export { default as transitions } from './transitions';
