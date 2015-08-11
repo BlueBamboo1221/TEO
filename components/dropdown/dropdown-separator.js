3897f9b0017d78432f719ea66061477a755b4459
root <fullstackdeveloper52@outlook.com>
2015-08-11 15:56:42 -0700

    dropdown-separator.js updated

--- components/dropdown/dropdown-separator.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Box } from '../base';
-
-const DropdownSeparator = () => (
-  <Box borderTop borderGray3 role="separator" />
-);
-
-export default DropdownSeparator;
