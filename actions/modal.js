3d5d0e9f7441fd4e6afce48f35b0d6ef3c3aa339
root <fullstackdeveloper52@outlook.com>
2015-11-12 15:24:06 -0800

    modal.js updated

--- actions/modal.js
+++
@@ -1,8 +0,0 @@
-import { createAction } from 'redux-actions';
-
-export const showModal = createAction('SHOW_MODAL', (type, props) => ({
-  modalType: type,
-  modalProps: { ...props },
-}));
-
-export const hideModal = createAction('HIDE_MODAL');
