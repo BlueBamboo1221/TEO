585d4fa76163be494b0e422da859c96b76c63c7d
root <fullstackdeveloper52@outlook.com>
2015-11-20 16:21:39 -0800

    index.js updated

--- components/dropdown/index.js
+++
@@ -1,3 +0,0 @@
-export { default as DropdownMenu } from './dropdown-menu';
-export { default as DropdownLink } from './dropdown-link';
-export { default as DropdownSeparator } from './dropdown-separator';
